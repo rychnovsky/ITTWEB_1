c4757f5d01e784b4f1d9a0b1a72657d068ed43dd
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2018-08-31 10:04:42 +0200

    Added /login page

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,11 @@
 /* GET home page */
+
+const title = 'Workout app';
+
 module.exports.index = function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title });
+};
+
+module.exports.login = function(req, res) {
+  res.render('login', { title });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,4 +5,6 @@ const ctrlMain = require('../controllers/main');
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
+router.get('/login', ctrlMain.login);
+
 module.exports = router;
